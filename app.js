0ea01364d559735b5f7c90d1265026f64d47d201
Djefdeg <djefdeg@hotmail.com>
2026-07-14 06:11:29 -0400

    Configure Express middleware

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+//Décoder les requetes en JSON
+app.use(express.json());
+//Décoder les requetes en HTML par un formulaire
+app.use(express.urlencoded({ extended: true }));
+//Afficher la méthode et l'URL
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 // Définition de la route principale
 app.get('/', (req, res) => {
   res.send('Bonjour depuis mon serveur Express !');
